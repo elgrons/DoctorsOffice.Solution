19d1df41fe8d85ad9246cdfd794cbba5234b69ce
Eliot Gronstal <eliot.lauren@gmail.com>
2023-03-15 11:55:02 -0700

    delete null patient rows manually from SQLWorkbench to get functionality from error messages.
    Co-authored-by: Kai Clausen <kaiclausen123@gmail.com>
    Co-Authored-by: Molly Donegan <mollyrdonegan@gmail.com>

--- DocOffice/Models/Doctor.cs
+++ DocOffice/Models/Doctor.cs
@@ -9,7 +9,7 @@ namespace DocOffice.Models
     public int DoctorId { get; set; }
     [Required(ErrorMessage = "The doctors's name can't be empty!")]
     public string DoctorName { get; set; }
-    [Range(1, int.MaxValue, ErrorMessage = "You must add a patient to a doctor. Have you added a doctor yet?")]
+    //[Range(1, int.MaxValue, ErrorMessage = "You must add a patient to a doctor. Have you added a doctor yet?")]
     public string DoctorTitle { get; set; }
     public List<DoctorPatient> JoinEntities { get; }
   }
--- DocOffice/Models/Patient.cs
+++ DocOffice/Models/Patient.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System;
 
@@ -6,8 +7,9 @@ namespace DocOffice.Models
   public class Patient
   {
     public int PatientId { get; set; }
+    [Required(ErrorMessage = "The patient name field can't be empty!")]
     public string PatientName { get; set; }
-
+    [Range(1, int.MaxValue, ErrorMessage = "You must add a doctor to a patient. Have you added a patient yet?")]
     public DateTime PatientBirthday { get; set; }
     public List<DoctorPatient> JoinEntities { get; }
   }
